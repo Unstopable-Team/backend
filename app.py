a9c7aa76e0140b47cffdee577858698331add92a
JuuisLe <henryledinhduy@gmail.com>
2021-03-19 11:29:43 +0200

    Updated

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-import os
 from flask import Flask
-from dotenv import load_dotenv
-
 import time
 
 import redis
 
-app = Flask(__name__)  
+app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
+
 
 def get_hit_count():
     retries = 5
@@ -19,6 +17,7 @@ def get_hit_count():
                 raise exc
             retries -= 1
             time.sleep(0.5)
+
 
 @app.route('/')
 def hello():
---
+++ test_pytest_work.py
@@ -0,0 +1,2 @@
+def test_if_py_test_work():
+    assert 5 == 5
