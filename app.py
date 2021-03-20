3b89358ea0257bdd0811d30f2d8bdb22011b0516
JuuisLe <henryledinhduy@gmail.com>
2021-03-20 09:29:03 +0200

    Added WebSocket and Web Socket Demo

--- app.py
+++ app.py
@@ -1,8 +1,48 @@
-from flask import Flask
+from flask import Flask, render_template, session, copy_current_request_context
+from flask_socketio import SocketIO, emit, disconnect
+from threading import Lock
 
+
+async_mode = None
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socket_ = SocketIO(app, async_mode=async_mode)
+thread = None
+thread_lock = Lock()
 
-
+# RestAPI endpoint
 @app.route('/')
-def hello():
-    return 'Hello Guys'
+def index():
+    return render_template('index.html', async_mode=socket_.async_mode)
+
+
+# Socket connection implementation
+@socket_.on('notification', namespace='/forecast')
+def test_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': message['data'], 'count': session['receive_count']})
+
+
+@socket_.on('critical_notification', namespace='/forecast')
+def test_broadcast_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': message['data'], 'count': session['receive_count']},
+         broadcast=True)
+
+
+@socket_.on('disconnect_request', namespace='/forecast')
+def disconnect_request():
+    @copy_current_request_context
+    def can_disconnect():
+        disconnect()
+
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my_response',
+         {'data': 'Disconnected!', 'count': session['receive_count']},
+         callback=can_disconnect)
+
+
+if __name__ == '__main__':
+    socket_.run(app, debug=True)
